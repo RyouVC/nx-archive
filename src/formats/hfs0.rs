2fa7bfc028c8f3f57aec69902005aea7939e2ec7
Cappy Ishihara <cappy@cappuchino.xyz>
2025-03-19 03:31:28 +0700

    XCI and HFS0 reading is now supported 🚀

--- src/formats/hfs0.rs
+++ src/formats/hfs0.rs
@@ -9,3 +9,138 @@
 
 use binrw::prelude::*;
 use std::io::{Read, Seek, SeekFrom};
+
+/// Nintendo Switch HFS0 (Hashed File System 0) header structure
+///
+/// This header is located at the beginning of an HFS0 archive file and contains:
+/// - A magic identifier "HFS0" (validated by binrw)
+///
+/// The "SHA-256 File System" or "HFS0" starts at offset 0x10000 in the Gamecard.
+/// The first 0x200 bytes act as a global header and
+/// represent the root partition which points to the other partitions
+/// ("normal", "logo", "update" and "secure").
+#[derive(Debug)]
+#[binrw]
+#[brw(little, magic = b"HFS0")]
+pub struct Hfs0Header {
+    // magic field is handled by binrw using the magic attribute
+    pub file_count: u32,
+    pub string_table_size: u32,
+    pub _reserved: u32,
+    #[br(count = file_count)]
+    pub file_entries: Vec<Hfs0Entry>,
+    /// String table - 00-padded to align the start of raw filedata with a sector/media unit boundary
+    #[br(pad_size_to = string_table_size)]
+    #[br(count = string_table_size)]
+    pub string_table: Vec<u8>,
+    // the rest is raw file data, which is not parsed by this struct
+    // due to memory constraints
+
+    // We will seek and read as needed
+}
+
+#[derive(Debug)]
+#[binrw]
+#[brw(little)]
+pub struct Hfs0Entry {
+    /// Offset of the file in data
+    pub offset: u64,
+    /// File size
+    pub size: u64,
+    /// Offset of filename in string table
+    pub filename_offset: u32,
+    /// Size of hashed region of file
+    /// (for HFS0s, this is the size of the pre-filedata portion,
+    /// for NCAs this is usually 0x200)
+    pub hashed_region_size: u32,
+    /// Reserved field
+    pub _reserved: u64,
+    /// SHA-256 hash of the first (size of hashed region) bytes of filedata
+    pub sha256: [u8; 0x20],
+}
+
+#[derive(Debug)]
+pub struct Hfs0File {
+    pub name: String,
+    pub size: u64,
+    /// Offset of the start of the file in the HFS0
+    ///
+    /// In our case, this offset is absolute to the start of the HFS0 file.
+    pub offset: u64,
+    pub hash: [u8; 0x20],
+}
+
+#[derive(Debug)]
+pub struct Hfs0<R: Read + Seek> {
+    pub header: Hfs0Header,
+    pub reader: R,
+}
+
+impl<R: Read + Seek> Hfs0<R> {
+    pub fn new(mut reader: R) -> BinResult<Self> {
+        let header = Hfs0Header::read(&mut reader)?;
+
+        Ok(Self { header, reader })
+    }
+
+    pub fn read_file(&mut self, file: &Hfs0File) -> Result<Vec<u8>, std::io::Error> {
+        self.reader.seek(SeekFrom::Start(file.offset))?;
+        let mut data = vec![0; file.hash.len()];
+        self.reader.read_exact(&mut data)?;
+        Ok(data)
+    }
+
+    pub fn get_files(&self) -> Vec<Hfs0File> {
+        self.header
+            .file_entries
+            .iter()
+            .map(|entry| {
+                let filename_bytes = &self.header.string_table[entry.filename_offset as usize..];
+                let end = filename_bytes
+                    .iter()
+                    .position(|&b| b == 0)
+                    .unwrap_or(filename_bytes.len());
+                let name = std::str::from_utf8(&filename_bytes[..end])
+                    .unwrap()
+                    .to_string();
+                
+                // Use the name we just extracted to call get_file()
+                // Since we know the file exists, unwrap is safe here
+                self.get_file(&name).unwrap()
+            })
+            .collect()
+    }
+
+    pub fn get_file(&self, name: &str) -> Option<Hfs0File> {
+        // Calculate the header size: 16 bytes for the HFS0 header fields + file entries + string table
+        let header_size = 16
+            + (self.header.file_entries.len() * std::mem::size_of::<Hfs0Entry>())
+            + self.header.string_table_size as usize;
+
+        self.header
+            .file_entries
+            .iter()
+            .find_map(|entry| {
+                let filename_bytes = &self.header.string_table[entry.filename_offset as usize..];
+                let end = filename_bytes
+                    .iter()
+                    .position(|&b| b == 0)
+                    .unwrap_or(filename_bytes.len());
+                let entry_name = std::str::from_utf8(&filename_bytes[..end])
+                    .unwrap()
+                    .to_string();
+                    
+                if entry_name == name {
+                    Some(Hfs0File {
+                        name: entry_name,
+                        size: entry.size,
+                        // Add header size to the offset to get the absolute file position
+                        offset: entry.offset + header_size as u64,
+                        hash: entry.sha256,
+                    })
+                } else {
+                    None
+                }
+            })
+    }
+}
--- src/formats/xci.rs
+++ src/formats/xci.rs
@@ -7,3 +7,415 @@
 //! and less wide. The data inside is stored in an eMMC chip, which is soldered onto the card.
 //! They are also coated with Denatonium Benzoate, a bittering agent, to discourage consumption.
 //! Just in case you (or your pet, or your little sibling) get any ideas. No, the game cards taste horrible (by design).
+
+// note to self: XCI files are massive, so we need to be veery careful with memory usage.
+// maybe consider using a buffered reader to read the file in chunks?
+// or, mmap the file and read it in chunks that way?
+use binrw::prelude::*;
+use std::io::{Read, Seek, SeekFrom};
+use tracing::trace;
+
+use crate::io::SubFile;
+
+use super::hfs0::{self, Hfs0};
+
+#[binrw]
+#[derive(Debug)]
+#[brw(little, repr = u8)]
+/// Size of the eMMC chip on the game card,
+/// base 2 logarithm of the size in gigabytes.
+pub enum RomSize {
+    Size1Gb = 0xFA,
+    Size2Gb = 0xF8,
+    Size4Gb = 0xF0,
+    Size8Gb = 0xE0,
+    Size16Gb = 0xE1,
+    Size32Gb = 0xE2,
+}
+
+#[binrw]
+#[derive(Debug)]
+#[brw(little, repr = u8)]
+/// Flags for the Game card
+/// These are used to determine extra features of the game card.
+pub enum GameCardFlags {
+    /// Boots to this game card's title automatically
+    /// on startup
+    AutoBoot = 0,
+    /// Don't ever record this card ever being launched
+    HistoryErase = 1,
+    /// This card is a service card,
+    /// used for debugging and repairs by
+    /// Nintendo
+    RepairTool = 2,
+    /// This card is Chinese region-locked,
+    /// and can only be played on Chinese Switches
+    /// (Terra region)
+    DifferentRegionCupToTerraDevice = 3,
+    /// This card is region-free, and can be played
+    /// globally on any Switch. Used for worldwide
+    /// game releases that contain multiple Asian locales.
+    /// i.e Southeast Asian releases
+    ///
+    /// These releases are usually labeled as "Asia" or "Multi-Language"
+    DifferentRegionCupToGlobalDevice = 4,
+    /// This card has a next-generation header format, used
+    /// for later Switch games
+    HasNewCardHeader = 7,
+    HasNewCardHeaderV2 = 8,
+}
+
+#[binrw]
+#[derive(Debug)]
+#[brw(little, repr = u64)]
+/// Firmware version for game card
+pub enum FirmwareVersion {
+    Development = 0,
+    Retail1_0_0 = 1,
+    Retail4_0_0 = 2,
+    Devel11_0_0 = 3,
+    Retail11_0_0 = 4,
+    Retail12_0_0 = 5,
+}
+
+#[binrw]
+#[derive(Debug)]
+#[brw(little, repr = u32)]
+/// Access control flags for game card
+pub enum AccessControlFlags {
+    /// No access control
+    TwentyFiveMhz = 0x00A10011,
+    /// 50mhz
+    FiftyMhz = 0x00A10012,
+}
+
+#[binrw]
+#[derive(Debug)]
+#[brw(little, repr = u8)]
+/// Compatibility type for game card
+pub enum CompatibilityType {
+    /// Normal game card
+    Normal = 0,
+    /// Chinese game card, compatible
+    /// with Chinese Switches
+    Terra = 1,
+}
+
+/// Size of a media sector
+pub const MEDIA_SIZE: u64 = 0x200;
+
+/// XCI Header structure
+#[derive(Debug, BinRead, BinWrite)]
+#[br(little)]
+pub struct XciHeader {
+    /// RSA-2048 signature over the header
+    pub signature: [u8; 0x100],
+    #[brw(magic = b"HEAD")]
+    /// `RomAreaStartPageAddress`
+    /// (in Gamecard page units, which are 0x200 bytes)
+    pub rom_area_offset: u32,
+    /// Offset to backup partition
+    /// (Should always be 0xFFFFFFFF)
+    pub backup_area_offset: u32,
+    /// Index for title key encryption key
+    /// TitleKeyDecIndex (high nibble) and KekIndex (low nibble)
+    pub title_kek_index: u8,
+    /// Size of the gamecard (0-7)
+    pub rom_size: RomSize,
+    /// Version of the gamecard header
+    pub gamecard_header_version: u8,
+    /// Flags for the gamecard
+    pub gamecard_flags: GameCardFlags,
+    /// Unique identifier for the game package
+    pub package_id: u64,
+    /// End offset of the valid data
+    /// (in Gamecard page units, which are 0x200 bytes)
+    pub valid_data_end_address: u32,
+    /// Additional gamecard info
+    _reserved: u8,
+    /// Flags2
+    pub gamecard_flags2: u8,
+    /// ApplicationIdListEntryCount
+    pub application_id_list_entry_count: u16,
+    /// Reversed IV
+    pub reversed_iv: [u8; 0x10],
+    /// PartitionFS header address
+    pub hfs0_offset: u64,
+    /// Size of the HFS0 header
+    pub hfs0_header_size: u64,
+    // HFS0 header hash
+    pub hfs0_header_hash: [u8; 0x20],
+    /// Hash of the InitialData
+    pub initial_data_hash: [u8; 0x20],
+    /// SelSec
+    pub sel_sec: u32,
+    /// SelT1Key
+    pub sel_t1_key: u32,
+    /// SelKey
+    pub sel_key: u32,
+    /// LimArea
+    /// (in Gamecard page units, which are 0x200 bytes)
+    pub lim_area: u32,
+    /// CardHeaderEncrypted data,
+    /// encrypted with AES-128-CBC
+    ///
+    /// We will not parse this field, as it is encrypted
+    pub card_header_encrypted: [u8; 0x70],
+}
+
+/// Gamecard Information structure
+#[derive(Debug, BinRead, BinWrite)]
+#[br(little)]
+pub struct CardHeaderEncryptedData {
+    /// Firmware version
+    pub firmware_version: FirmwareVersion,
+    /// Access control flags
+    pub access_control_flags: AccessControlFlags,
+    /// Read wait time
+    ///
+    /// Most of the time this is always
+    /// 0x1388
+    pub read_wait_time: u32,
+    /// Read wait time 2
+    ///
+    /// Most of the time this is always 0
+    pub read_wait_time2: u32,
+    /// Write wait time
+    ///
+    /// Most of the time this is always 0
+    pub write_wait_time: u32,
+    /// Write wait time 2
+    ///
+    /// Most of the time this is always 0
+    pub write_wait_time2: u32,
+    /// Firmware mode
+    ///
+    /// (The SDK Addon version for this game)
+    pub firmware_mode: u32,
+    /// Minimal version for this game?
+    pub update_partition_version: u32,
+    /// Reserved
+    pub _reserved1: [u8; 0x3],
+    /// Hash of the update partition
+    pub update_partition_hash: u64,
+    /// Update partition ID
+    ///
+    /// This should always be 0x0100000000000816
+    pub update_partition_id: u64,
+    /// Empty field 2
+    pub empty2: [u8; 0x38],
+}
+
+/// Gamecard Certificate structure
+#[binrw]
+#[derive(Debug)]
+#[br(little)]
+pub struct GamecardCertificate {
+    /// RSA-2048 signature
+    pub signature: [u8; 0x100],
+    #[brw(magic = b"CERT")]
+    /// Unknown data 1
+    pub unknown1: [u8; 0x10],
+    /// Unknown data 2
+    pub unknown2: [u8; 0xA],
+    /// Certificate data
+    pub data: [u8; 0xD6],
+}
+
+/// XCI file representation
+pub struct Xci<R: Read + Seek> {
+    /// Reader for the XCI file
+    reader: R,
+    /// XCI header
+    pub header: XciHeader,
+    /// Optional key area for "full" XCI files
+    pub key_area: Option<Vec<u8>>,
+    /// Gamecard info
+    // pub gamecard_info: CardHeaderEncryptedData,
+    /// Gamecard certificate
+    pub gamecard_cert: Option<GamecardCertificate>,
+}
+
+/// Key Area found in "full" XCI files
+#[derive(Debug)]
+pub struct KeyArea {
+    /// Package ID (same as in header)
+    pub package_id: u64,
+    /// Challenge Response Auth Data
+    pub challenge_response_auth_data: [u8; 0x10],
+    /// Challenge Response Auth MAC
+    pub challenge_response_auth_mac: [u8; 0x10],
+    /// Challenge Response Auth Nonce
+    pub challenge_response_auth_nonce: [u8; 0x10],
+    /// Title key 1
+    pub title_key1: [u8; 0x8],
+    /// Title key 2
+    pub title_key2: [u8; 0x8],
+}
+
+impl<R: Read + Seek> Xci<R> {
+    /// Creates a new XCI instance from a reader
+    pub fn new(mut reader: R) -> binrw::BinResult<Self> {
+        // Check if this is a "full" XCI
+        // (full XCIs contain a 0x1000 key area, usually)
+        // unreadable directly in most r/w operations
+        let is_full_xci = {
+            trace!("Checking if this is a full XCI");
+            // Read at 0x100 first, we're checking for a trimmed XCI
+            // trimmed XCIs will have a header at 0x100
+            reader.seek(SeekFrom::Start(0x100))?;
+            let mut magic = [0u8; 4];
+            reader.read_exact(&mut magic)?;
+            let at_0x100 = b"HEAD"[..] == magic[..];
+            trace!("Found magic at 0x100?: {}", at_0x100);
+
+            if at_0x100 {
+                trace!("This is a trimmed XCI, there's no key area");
+                !at_0x100
+            } else {
+                // Let's go to 0x200
+                // full XCIs will have a 0x1000 key area before the XciHeader
+                reader.seek(SeekFrom::Start(0x1100))?;
+                reader.read_exact(&mut magic)?;
+                let at_0x1100 = b"HEAD"[..] == magic[..];
+                trace!("Found magic at 0x1100?: {}", at_0x1100);
+                at_0x1100
+            }
+        };
+
+        // Set up the key area if this is a full XCI
+        let key_area = if is_full_xci {
+            let mut key_data = vec![0u8; 0x1000];
+            reader.seek(SeekFrom::Start(0))?;
+            reader.read_exact(&mut key_data)?;
+            Some(key_data)
+        } else {
+            None
+        };
+
+        trace!("full XCI? {}", is_full_xci);
+
+        // Determine header offset
+        let header_offset = if is_full_xci { 0x100 } else { 0 };
+
+        let mut magic_area = [0u8; 4];
+        reader.seek(SeekFrom::Start(header_offset))?;
+        reader.read_exact(&mut magic_area)?;
+        reader.seek(SeekFrom::Start(header_offset))?;
+
+        // Read the header
+        reader.seek(SeekFrom::Start(header_offset))?;
+        let header: XciHeader = reader.read_le()?;
+
+        // Read gamecard info
+        // Full XCIs would be at 0x200, in this case 0x1100 + 0x100
+        // Trimmed XCIs would be at 0x100, so the header offset will be 0
+        // reader.seek(SeekFrom::Start(header_offset + 0x100))?;
+        // let gamecard_info: CardHeaderEncryptedData = reader.read_le()?;
+
+        // Read gamecard certificate
+        // Full XCIs would have this offset at 0x8000,
+        // so trimmed XCIs would have this at 0x7000
+        reader.seek(SeekFrom::Start(header_offset + 0x7000))?;
+        // Attempt to read the gamecard certificate but don't fail if it doesn't exist
+        let gamecard_cert = match reader.read_le::<GamecardCertificate>() {
+            Ok(cert) => Some(cert),
+            Err(err) => {
+                tracing::warn!("Failed to read gamecard certificate: {}", err);
+                None
+            }
+        };
+
+        Ok(Xci {
+            reader,
+            header,
+            key_area,
+            gamecard_cert,
+        })
+    }
+
+    /// Gets the offset to the HFS0 partition
+    pub fn get_hfs0_offset(&self) -> u64 {
+        if self.key_area.is_some() {
+            (0x1000 + self.header.hfs0_offset).into()
+        } else {
+            self.header.hfs0_offset.into()
+        }
+    }
+
+    /// Reads the HFS0 header
+    pub fn read_hfs0_header(&mut self) -> binrw::BinResult<Hfs0<SubFile<&mut R>>> {
+        let hfs0_offset = self.get_hfs0_offset();
+        self.reader.seek(SeekFrom::Start(hfs0_offset))?;
+
+        let subfile = SubFile::new(
+            &mut self.reader,
+            hfs0_offset,
+            hfs0_offset + self.header.hfs0_header_size,
+        );
+        let hfs0 = Hfs0::new(subfile)?;
+
+        trace!("HFS0 header read successfully");
+
+        Ok(hfs0)
+    }
+
+    #[tracing::instrument(skip(self), level = "trace")]
+    pub fn open_hfs0_partition(
+        &mut self,
+        part_name: &str,
+    ) -> binrw::BinResult<Option<Hfs0<SubFile<&mut R>>>> {
+        let hfs0_header = self.read_hfs0_header()?;
+
+        // trace!("Attempting to open HFS0 partition: {}", part_name);
+        let part = hfs0_header.get_file(part_name);
+
+        if let Some(file) = part {
+            trace!("Attempting to open HFS0 partition: {}", part_name);
+            // Calculate the absolute offset in the file
+            let hfs0_offset = self.get_hfs0_offset();
+            let start_offset = hfs0_offset + file.offset;
+            let end_offset = start_offset + file.size;
+
+            let subfile = SubFile::new(&mut self.reader, start_offset, end_offset);
+
+            let part = Hfs0::new(subfile)?;
+
+            trace!("HFS0 partition opened successfully");
+
+            return Ok(Some(part));
+        }
+
+        Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tracing_test::traced_test;
+
+    use super::*;
+    use std::fs::File;
+
+    #[test]
+    #[traced_test]
+    fn test_xci() {
+        // test file from Ace Attorney Investigations Collection
+        // thank you Edgeworth for being my guinea pig
+        let file = File::open("dump/aaic.xci").unwrap();
+        let mut xci = Xci::new(file).unwrap();
+
+        println!("{:#?}", xci.header);
+        trace!("HFS Header Offset {:02X?}", xci.header.hfs0_offset);
+        // println!("{:#?}", xci.gamecard_info);
+        println!("{:?}", xci.gamecard_cert);
+
+        // xci.read_hfs0_header().unwrap();
+        let normal_part = xci.open_hfs0_partition("secure").unwrap().unwrap();
+
+        let files = normal_part.get_files();
+        for file in &files {
+            println!("File: {:?}", file);
+        }
+    }
+}
